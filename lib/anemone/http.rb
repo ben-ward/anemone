d77acabb00e1b8ab633a9d456a0e75db218094b8
Ben Ward <ben@mauilabs.com>
2015-01-07 15:42:44 -1000

    Reverting

--- lib/anemone/http.rb
+++ lib/anemone/http.rb
@@ -6,12 +6,12 @@ module Anemone
   class HTTP
     # Maximum number of redirects to follow on each get_response
     REDIRECT_LIMIT = 5
-    RETRY_LIMIT = 6
-    
+
     # CookieStore for this HTTP client
     attr_reader :cookie_store
 
     def initialize(opts = {})
+      @connections = {}
       @opts = opts
       @cookie_store = CookieStore.new(@opts[:cookies])
     end
@@ -32,9 +32,8 @@ module Anemone
       begin
         url = URI(url) unless url.is_a?(URI)
         pages = []
-        get(url, referer) do |response, headers, code, location, redirect_to, response_time|
-          pages << Page.new(location, :body => response,
-                                      :headers => headers,
+        get(url, referer) do |response, code, location, redirect_to, response_time|
+          pages << Page.new(location, :body => response.body.dup,
                                       :code => code,
                                       :headers => response.to_hash,
                                       :referer => referer,
@@ -49,8 +48,7 @@ module Anemone
           puts e.inspect
           puts e.backtrace
         end
-        pages ||= []
-        return pages << Page.new(url, :error => e)
+        return [Page.new(url, :error => e)]
       end
     end
 
@@ -77,6 +75,20 @@ module Anemone
     end
 
     #
+    # The proxy address string
+    #
+    def proxy_host
+      @opts[:proxy_host]
+    end
+
+    #
+    # The proxy port
+    #
+    def proxy_port
+      @opts[:proxy_port]
+    end
+
+    #
     # HTTP read timeout in seconds
     #
     def read_timeout
@@ -98,10 +110,10 @@ module Anemone
           # request url
           loc = url.merge(loc) if loc.relative?
 
-          response, headers, response_time, response_code, redirect_to = get_response(loc, referer)
-          
-          yield response, headers, Integer(response_code), loc, redirect_to, response_time
-          
+          response, response_time = get_response(loc, referer)
+          code = Integer(response.code)
+          redirect_to = response.is_a?(Net::HTTPRedirection) ? URI(response['location']).normalize : nil
+          yield response, code, loc, redirect_to, response_time
           limit -= 1
       end while (loc = redirect_to) && allowed?(redirect_to, url) && limit > 0
     end
@@ -116,36 +128,48 @@ module Anemone
       opts['User-Agent'] = user_agent if user_agent
       opts['Referer'] = referer.to_s if referer
       opts['Cookie'] = @cookie_store.to_s unless @cookie_store.empty? || (!accept_cookies? && @opts[:cookies].nil?)
-      opts['read_timeout'] = read_timeout if !!read_timeout
-      opts['redirect'] = false
-      redirect_to = nil
 
       retries = 0
       begin
         start = Time.now()
-        
-        begin
-          resource = open(url, opts)
-        rescue OpenURI::HTTPRedirect => e_redirect
-          resource = e_redirect.io
-          redirect_to = e_redirect.uri
-        rescue OpenURI::HTTPError => e_http
-          resource = e_http.io
-        end
-
+        # format request
+        req = Net::HTTP::Get.new(full_path, opts)
+        # HTTP Basic authentication
+        req.basic_auth url.user, url.password if url.user
+        response = connection(url).request(req)
         finish = Time.now()
         response_time = ((finish - start) * 1000).round
-        @cookie_store.merge!(resource.meta['set-cookie']) if accept_cookies?
-        return resource.read, resource.meta, response_time, resource.status.shift, redirect_to
-
-      rescue Timeout::Error, EOFError, Errno::ECONNREFUSED, Errno::ETIMEDOUT, Errno::ECONNRESET => e
+        @cookie_store.merge!(response['Set-Cookie']) if accept_cookies?
+        return response, response_time
+      rescue Timeout::Error, Net::HTTPBadResponse, EOFError => e
+        puts e.inspect if verbose?
+        refresh_connection(url)
         retries += 1
-        puts "[anemone] Retrying ##{retries} on url #{url} because of: #{e.inspect}" if verbose?
-        sleep(3 ^ retries)
-        retry unless retries > RETRY_LIMIT
-      ensure
-        resource.close if !resource.nil? && !resource.closed?
+        retry unless retries > 3
       end
+    end
+
+    def connection(url)
+      @connections[url.host] ||= {}
+
+      if conn = @connections[url.host][url.port]
+        return conn
+      end
+
+      refresh_connection url
+    end
+
+    def refresh_connection(url)
+      http = Net::HTTP.new(url.host, url.port, proxy_host, proxy_port)
+
+      http.read_timeout = read_timeout if !!read_timeout
+
+      if url.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+
+      @connections[url.host][url.port] = http.start 
     end
 
     def verbose?
@@ -158,5 +182,6 @@ module Anemone
     def allowed?(to_url, from_url)
       to_url.host.nil? || (to_url.host == from_url.host)
     end
+
   end
 end
